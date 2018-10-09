b43efb415dcc6bdc95abfde2c2fa6af3492565bd
John Paxton <pax@speedingplanet.com>
2018-10-09 09:00:15 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import * as data from './json/class-data';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <main className="container">
+          <header>
+            <h1>Intuit Banking</h1>
+            <ul className="list-inline">
+              {/* None of these have defined routes at the moment! */}
+              <li className="list-inline-item"><Link to="/">Home</Link></li>
+              <li className="list-inline-item"><Link to="/experiments">Experiments</Link></li>
+              <li className="list-inline-item"><Link to="/categories">Categories</Link></li>
+              <li className="list-inline-item"><Link to="/payees">Payees</Link></li>
+            </ul>
+            <hr />
+          </header>
+        </main>
+      </Router>
+
     );
   }
 }
